fbfff18bfdf7a9502290c216fe52d344d6ff30d1
Pablo Mendez <pablom@complex.com>
2015-09-15 18:57:53 -0400

    UPDATING ADD-ON INFORMATION

--- plugin.video.complex-tv/default.py
+++ plugin.video.complex-tv/default.py
@@ -1,3 +1,9 @@
+# Author : Pablo Mendez
+# Company: Complex Media Inc
+# 
+# This addon is for free and redistribution . 	
+#
+
 import urllib2
 import json
 import sys
@@ -8,23 +14,30 @@ addon_handle = int(sys.argv[1])
 xbmcplugin.setContent(addon_handle, 'movies')
 
 #get the video list from oayalla.
-#response = urllib2.urlopen('http://cdn-api.ooyala.com/v2/syndications/88af49794d964697ac4bdefd076f95e0/feed?pcode=ttYTMxOmh-6nk2wKZI-ih5W1iWxx')
 complexNews = 'http://cdn-api.ooyala.com/v2/syndications/88af49794d964697ac4bdefd076f95e0/feed?pcode=ttYTMxOmh-6nk2wKZI-ih5W1iWxx'
 
-complexCDN = 'http://cdn-api.ooyala.com/v2/syndications/6fa7cb50ad2e40bda2007743062957b1/feed?pcode=ttYTMxOmh-6nk2wKZI-ih5W1iWxx'
+#complexCDN = 'http://cdn-api.ooyala.com/v2/syndications/6fa7cb50ad2e40bda2007743062957b1/feed?pcode=ttYTMxOmh-6nk2wKZI-ih5W1iWxx'
 
-response = urllib2.urlopen(complexCDN)
+response = urllib2.urlopen(complexNews)
 
 html = response.read()
 stream =  json.loads(html)
 
+# add the videos to the playlist.
+
 for videos in stream['media']:
+
     li = xbmcgui.ListItem(videos['title'], iconImage='DefaultVideo.png', thumbnailImage=videos['thumbURL'])
+    
     li.setProperty('fanart_image', videos['thumbURL'])
+    
     li.addStreamInfo('video', { 'codec': 'h264', 'aspect': 1.78, 'width': 1280, 'height': 720 })
+    
     xbmcplugin.addDirectoryItem(handle=addon_handle, url=videos['videoURL'], listitem=li)
 
 xbmcplugin.endOfDirectory(addon_handle)
+
+## Settings about to develop
 
 #my_addon = xbmcaddon.Addon()
 #my_setting = my_addon.getSetting('complex_setting') # returns the string 'true' or 'false'
